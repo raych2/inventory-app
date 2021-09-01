69e3822477a4de0efb9baf10cfeb60ae7054926d
raych2 <rtongco@gmail.com>
2021-09-01 14:21:18 -0700

    Add item, vendor, and category routes and delete userRouter

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,90 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// Require controller modules.
+const item_controller = require("../controllers/itemController");
+const vendor_controller = require("../controllers/vendorController");
+const category_controller = require("../controllers/categoryController");
+
+/// ITEM ROUTES ///
+
+// GET home page.
+router.get("/", item_controller.index);
+
+// GET request for creating a Item. NOTE This must come before routes that display Item (uses id).
+router.get("/item/create", item_controller.item_create_get);
+
+// POST request for creating Item.
+router.post("/item/create", item_controller.item_create_post);
+
+// GET request to delete Item.
+router.get("/item/:id/delete", item_controller.item_delete_get);
+
+// POST request to delete Item.
+router.post("/item/:id/delete", item_controller.item_delete_post);
+
+// GET request to update Item.
+router.get("/item/:id/update", item_controller.item_update_get);
+
+// POST request to update Item.
+router.post("/item/:id/update", item_controller.item_update_post);
+
+// GET request for one Item.
+router.get("/item/:id", item_controller.item_detail);
+
+// GET request for list of all Item items.
+router.get("/items", item_controller.item_list);
+
+/// VENDOR ROUTES ///
+
+// GET request for creating Vendor. NOTE This must come before route for id (i.e. display vendor).
+router.get("/vendor/create", vendor_controller.vendor_create_get);
+
+// POST request for creating Vendor.
+router.post("/vendor/create", vendor_controller.vendor_create_post);
+
+// GET request to delete Vendor.
+router.get("/vendor/:id/delete", vendor_controller.vendor_delete_get);
+
+// POST request to delete Vendor.
+router.post("/vendor/:id/delete", vendor_controller.vendor_delete_post);
+
+// GET request to update Vendor.
+router.get("/vendor/:id/update", vendor_controller.vendor_update_get);
+
+// POST request to update Vendor.
+router.post("/vendor/:id/update", vendor_controller.vendor_update_post);
+
+// GET request for one Vendor.
+router.get("/vendor/:id", vendor_controller.vendor_detail);
+
+// GET request for list of all Vendors.
+router.get("/vendors", vendor_controller.vendor_list);
+
+/// CATEGORY ROUTES ///
+
+// GET request for creating a Category. NOTE This must come before route that displays Category (uses id).
+router.get("/category/create", category_controller.category_create_get);
+
+//POST request for creating Category.
+router.post("/category/create", category_controller.category_create_post);
+
+// GET request to delete Category.
+router.get("/category/:id/delete", category_controller.category_delete_get);
+
+// POST request to delete Category.
+router.post("/category/:id/delete", category_controller.category_delete_post);
+
+// GET request to update Category.
+router.get("/category/:id/update", category_controller.category_update_get);
+
+// POST request to update Category.
+router.post("/category/:id/update", category_controller.category_update_post);
+
+// GET request for one Category.
+router.get("/category/:id", category_controller.category_detail);
+
+// GET request for list of all Category.
+router.get("/categories", category_controller.category_list);
 
 module.exports = router;
